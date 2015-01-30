7f6858899d67944f460c8dd1fc89045bddbe027d
Grant George <grantelliottgeorge@gmail.com>
2015-01-30 16:32:19 -0500

    Add MY_Controller; enable CORS

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -1,22 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-/**
- * Example
- *
- * This is an example of a few basic user interaction methods you could use
- * all done with a hardcoded array.
- *
- * @package   CodeIgniter
- * @subpackage  Rest Server
- * @category  Controller
- * @author    Phil Sturgeon
- * @link    http://philsturgeon.co.uk/code/
-*/
-
-// This can be removed if you use __autoload() in config.php OR use Modular Extensions
-require APPPATH.'/libraries/REST_Controller.php';
-
-class Test extends REST_Controller
+class Test extends MY_Controller
 {
   function __construct()
   {
